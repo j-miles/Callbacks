27f57a98df6526124c06a849d6b95faa22283c65
Jackie <jaxmiles@gmail.com>
2017-03-09 20:58:24 +0000

    refactored function with forEach

--- callbacksone.js
+++ callbacksone.js
@@ -1,11 +1,27 @@
 // The second argument/parameter is expected to be a function
-function findWaldo(arr, found) {
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] === "Waldo") {
+// function findWaldo(arr, found) {
+//   for (var i = 0; i < arr.length; i++) {
+//     if (arr[i] === "Waldo") {
 
-      found(i);   // execute callback
+//       found(i);   // execute callback
+//     }
+//   }
+// }
+
+// function actionWhenFound(index) {
+//   console.log("Found Waldo at " + index + "!");
+// }
+
+// findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
+
+
+function findWaldo(array, found) {
+  array.forEach(function (elm,index,array) {
+    if (elm === "Waldo") {
+
+      found(index);   // execute callback
     }
-  }
+  })
 }
 
 function actionWhenFound(index) {
